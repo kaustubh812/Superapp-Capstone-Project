b1e1457afbeef5785fef87fa25bccdaf9cf09d4e
kaustubh812 <watanekaustubh@gmail.com>
2023-12-04 22:45:04 +0530

    .

--- src/Components/Home/Weather.jsx
+++ src/Components/Home/Weather.jsx
@@ -7,18 +7,24 @@ import wind from "../../assets/wind.png";
 // Weather component to display current weather information
 const Weather = () => {
   // State variables for weather data, date, and time
-  const [weather, setWeather] = useState(false);
+  const [weather, setWeather] = useState(null);
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
 
   // Fetch weather data from the API on component mount
   useEffect(() => {
     const fetchWeather = async () => {
-      const response = await fetch(
-        "http://api.weatherapi.com/v1/current.json?key=987de39fe8924052ada80850232502&q=London&aqi=no"
-      );
-      const data = await response.json();
-      setWeather(data);
+      try {
+        // Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual API key
+        const apiKey = '04e4adac5abd87b85d439261a8593250';
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`
+        );
+        const data = await response.json();
+        setWeather(data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
     };
     fetchWeather();
   }, []);
@@ -33,7 +39,7 @@ const Weather = () => {
     minutes = minutes < 10 ? "0" + minutes : minutes;
     const formattedTime = hours + ":" + minutes + " " + ampm;
     setTime(formattedTime);
-  });
+  }, []);
 
   // Set current date in DD-MM-YYYY format
   useEffect(() => {
@@ -43,7 +49,7 @@ const Weather = () => {
     let dd = String(today.getDate()).padStart(2, "0");
     const formattedDate = dd + "-" + mm + "-" + yyyy;
     setDate(formattedDate);
-  });
+  }, []);
 
   return (
     <div
@@ -88,14 +94,18 @@ const Weather = () => {
           >
             {/* Section 1 - Weather condition */}
             <div style={{ marginTop: "-6.4px" }}>
-              <img
-                src={weather.current.condition.icon}
-                style={{ width: "52.48px", height: "48px", margin: "0 6.4px 0 25.6px" }}
-                alt="Weather Condition"
-              />
-              <p style={{ fontSize: "16px", margin: "0px 0px 0px 12.8px", marginTop: "3.2px" }}>
-                {weather.current.condition.text}
-              </p>
+              {weather.weather && weather.weather[0] && (
+                <img
+                  src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                  style={{ width: "52.48px", height: "48px", margin: "0 6.4px 0 25.6px" }}
+                  alt="Weather Condition"
+                />
+              )}
+              {weather.weather && weather.weather[0] && (
+                <p style={{ fontSize: "16px", margin: "0px 0px 0px 12.8px", marginTop: "3.2px" }}>
+                  {weather.weather[0].description}
+                </p>
+              )}
             </div>
 
             {/* Line separator */}
@@ -103,22 +113,26 @@ const Weather = () => {
 
             {/* Section 2 - Temperature and Pressure */}
             <div>
-              <p
-                style={{
-                  marginBottom: "3.84px",
-                  fontSize: "28.8px",
-                  marginTop: "3.84px",
-                  marginLeft: "10px"
-                }}
-              >
-                <span >{weather.current.temp_c}</span>°C
-              </p>
-              <div style={{ display: "flex", alignItems: "center", fontSize: "13px" }}>
-                <img src={pressure} style={{ height: "17.92px", width: "9.6px" }} alt="pressure" />
-                <p style={{ marginLeft: "4.8px", marginBottom: "3.84px", marginTop: "3.84px" }}>
-                  {weather.current.pressure_mb} mbar <br /> pressure
+              {weather.main && weather.main.temp && (
+                <p
+                  style={{
+                    marginBottom: "3.84px",
+                    fontSize: "28.8px",
+                    marginTop: "3.84px",
+                    marginLeft: "10px",
+                  }}
+                >
+                  <span>{weather.main.temp}</span>°C
                 </p>
-              </div>
+              )}
+              {weather.main && weather.main.pressure && (
+                <div style={{ display: "flex", alignItems: "center", fontSize: "13px" }}>
+                  <img src={pressure} style={{ height: "17.92px", width: "9.6px" }} alt="pressure" />
+                  <p style={{ marginLeft: "4.8px", marginBottom: "3.84px", marginTop: "3.84px" }}>
+                    {weather.main.pressure} mbar <br /> pressure
+                  </p>
+                </div>
+              )}
             </div>
 
             {/* Line separator */}
@@ -126,35 +140,39 @@ const Weather = () => {
 
             {/* Section 3 - Wind and Humidity */}
             <div>
-              <div style={{ display: "flex", alignItems: "center", fontSize: "13px" }}>
-                <img src={wind} style={{ height: "16px", width: "16px" }} alt="wind" />
-                <p
-                  style={{
-                    marginBottom: "3.84px",
-                    marginTop: "3.84px",
-                    marginLeft: "4.8px",
-                  }}
-                >
-                  {weather.current.wind_kph} km/h <br /> wind
-                </p>
-              </div>
+              {weather.wind && weather.wind.speed && (
+                <div style={{ display: "flex", alignItems: "center", fontSize: "13px" }}>
+                  <img src={wind} style={{ height: "16px", width: "16px" }} alt="wind" />
+                  <p
+                    style={{
+                      marginBottom: "3.84px",
+                      marginTop: "3.84px",
+                      marginLeft: "4.8px",
+                    }}
+                  >
+                    {weather.wind.speed} km/h <br /> wind
+                  </p>
+                </div>
+              )}
 
-              <div style={{ display: "flex", alignItems: "center", fontSize: "13px" }}>
-                <img src={humidity} style={{ height: "17.92px", width: "11.52px" }} alt="humidity" />
-                <p
-                  style={{
-                    marginBottom: "3.84px",
-                    marginTop: "6.4px",
-                    marginLeft: "4.8px",
-                  }}
-                >
-                  {weather.current.humidity} % <br /> humidity
-                </p>
-              </div>
+              {weather.main && weather.main.humidity && (
+                <div style={{ display: "flex", alignItems: "center", fontSize: "13px" }}>
+                  <img src={humidity} style={{ height: "17.92px", width: "11.52px" }} alt="humidity" />
+                  <p
+                    style={{
+                      marginBottom: "3.84px",
+                      marginTop: "6.4px",
+                      marginLeft: "4.8px",
+                    }}
+                  >
+                    {weather.main.humidity} % <br /> humidity
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         ) : (
-          <></>
+          <p>Loading...</p>
         )}
       </div>
     </div>
